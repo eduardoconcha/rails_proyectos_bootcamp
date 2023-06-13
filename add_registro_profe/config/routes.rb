195a1ab13ee581cb3745db3c0f733f1584a84eba
eduardoconcha <ejconcha@hotmail.com>
2023-06-13 17:45:14 -0400

    13/06/2023

---
+++ add_registro_profe/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/create'
+  post 'pages/create'
+  
+  root 'pages#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
